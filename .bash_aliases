fe5bc766c0dfb8ca1984e6236453ff8af83d1959
Liam Whan <liamwhan@eq8.ai>
2023-06-13 14:10:23 +1000

    Update to copy aliases into home etc.

---
+++ .bash_aliases
@@ -0,0 +1,7 @@
+#!/bin/bash
+alias gs="git status"
+alias gb="git branch"
+alias gco="git checkout"
+alias gl="git pull"
+alias gp="git push"
+alias gup="git branch --set-upstream-to="
--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-alias gs="git status"
-alias gb="git branch"
-alias gco="git checkout"
-alias gl="git pull"
-alias gp="git push"
-alias gup="git branch --set-upstream-to="
+cp ./.bash_aliases ~/.bash_aliases
 
 # change time zone
 sudo ln -fs /usr/share/zoneinfo/Australia/Sydney /etc/localtime
