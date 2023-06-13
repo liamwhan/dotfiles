62fbd5e4f2b2cce20ea4f2c1722cefb8d570085f
Liam Whan <liamwhan@eq8.ai>
2023-06-13 14:00:23 +1000

    Update hashbang and add TZ change

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 alias gs="git status"
 alias gb="git branch"
@@ -7,3 +7,6 @@ alias gl="git pull"
 alias gp="git push"
 alias gup="git branch --set-upstream-to="
 
+# change time zone
+sudo ln -fs /usr/share/zoneinfo/Australia/Sydney /etc/localtime
+sudo dpkg-reconfigure --frontend noninteractive tzdata
